ed01aa39e612103481b9cf77d78ff027129899f1
Dimitar Georgiev <dimitarg92@gmail.com>
2022-01-26 10:00:18 +0200

    Lab 1

---
+++ FundamentalsCS/DataTypesAndVariables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesAndVariables
+{
+    class ConvertMetersToKilometers
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+
+            double kilometers = meters / 1000.0f;
+
+            Console.WriteLine($"{kilometers:f2}");
+
+        }
+    }
+}
