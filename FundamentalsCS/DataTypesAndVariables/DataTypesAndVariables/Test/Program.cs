a3bfdc1b6341b71a4acd1295c86b50248a53268c
Dimitar Georgiev <dimitarg92@gmail.com>
2022-02-01 07:41:54 +0200

    - Exercises

---
+++ FundamentalsCS/DataTypesAndVariables/DataTypesAndVariables/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 1;
+            var b = "1";
+
+            Console.WriteLine(a + b);
+
+        }
+    }
+}
